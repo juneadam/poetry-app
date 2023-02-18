d73389a26cba36d4240c5e2691e54d5c23a22273
June Balter <junedoeswork@gmail.com>
2023-02-17 20:57:51 -0600

    combined test_server and test_utils into one file, tests.py, for more accurate coverage reporting

--- tests.py
+++ tests.py
@@ -7,6 +7,7 @@ from server import app
 import server
 from model import connect_to_db, db
 from seed_database import seed_database
+import utils
 
 
 # ============ Testing simple GET request HTML renders ============ #
@@ -355,6 +356,56 @@ class FlaskTestsPoemsJSON(TestCase):
             self.assertEqual(result.status_code, 200, result.data)
             assert b'error' in result.data
 
+
+# ============ UTILS.PY ============ #
+# ============ testing the logic of utility functions ============ #
+
+class FlaskTestsUtilsPayload(TestCase):
+    """Tests utility functions."""
+
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def tearDown(self):
+        pass
+
+    def test_get_payload_4params(self):
+
+        assert utils.get_payload(author="shakespeare", title="sonnet", lines="thence", linecount="14") == ('author,title,lines,linecount','shakespeare;sonnet;thence;14')
+    
+    def test_get_payload_2params(self):
+
+        assert utils.get_payload(author="byron", linecount="20") == ('author,linecount','byron;20')
+
+    def test_get_payload_0params(self):
+
+        assert utils.get_payload() == ('','')
+
+
+class FlaskTestsUtilsLogicFuncs(TestCase):
+    """Tests utility functions."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_range_modifier(self):
+
+        for linecount in range(1,4):
+            assert utils.range_modifier(linecount) == 5
+        for linecount in range(4,10):
+            assert utils.range_modifier(linecount) == 2
+
+    def test_form_easter_egg(self):
+
+        assert utils.form_easter_egg(3) == "Haiku: "
+        assert utils.form_easter_egg(5) == "Limerick: "
+        assert utils.form_easter_egg(14) == "Sonnet: "
+        assert utils.form_easter_egg(19) == "Villanelle: "
+        assert utils.form_easter_egg(100) == "Cento: "
 
 if __name__ == "__main__":
     os.system('dropdb testdb')
